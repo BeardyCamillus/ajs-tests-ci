3a3b31b70ab4501eacb98b9f50b2855e2a279181
BeardyCamillus <beardy.camillus@gmail.com>
2023-09-15 18:27:12 +0300

    add functions

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,22 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+function healthState(params) {
+    const { health } = params;
+
+    if (health > 50) {
+        return 'healthy';
+    } else if (health < 15) {
+        return 'critical';
+    } else {
+        return 'wounded';
+    }
+}
+
+function sortByHealthDesc(heroesHealth) {
+    heroesHealth.sort((a, b) => a.health - b.health).reverse();
+    return heroesHealth;
+}
+
+export { sortByHealthDesc, healthState };
